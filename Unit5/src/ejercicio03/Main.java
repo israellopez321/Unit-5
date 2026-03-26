873c36f20948b6eaf10d7c9831e00f17ed45fa57
israellopez321 <israel.lopez@iesnervion.es>
2026-03-26 12:49:20 +0100

    Ejercicio 3 Herencias

--- Unit5/src/ejercicio03/Main.java
+++ Unit5/src/ejercicio03/Main.java
@@ -2,5 +2,21 @@ package ejercicio03;
 
 public class Main {
 
+	
 
+	public static void main(String[] args) {
+		
+		Productos productoUno = new Productos("pecera", 12);
+		
+		Perecedero productoDos = new Perecedero("platano", 12, 2);
+		
+		NoPerecedero productoTres = new NoPerecedero("piedra", 12, "duro");
+		
+		System.out.println(productoUno.calcular(4));
+		
+		System.out.println(productoDos.calcular(4));
+		
+		System.out.println(productoTres.calcular(4));
+		
+	}
 }
